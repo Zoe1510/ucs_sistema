93a8a487a36e685cc1460da1d3fadfbc966d969c
zoe1510 <patriciabermudez465@gmail.com>
2017-11-20 00:37:05 -0400

    bd again

--- UCS_NODO_FGC/Hacer_reportes.Designer.cs
+++ UCS_NODO_FGC/Hacer_reportes.Designer.cs
@@ -28,11 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Panel_cabecera = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape7 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.Panel_cabecera.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel_cabecera
+            // 
+            this.Panel_cabecera.BackColor = System.Drawing.Color.MidnightBlue;
+            this.Panel_cabecera.BackgroundImage = global::UCS_NODO_FGC.Properties.Resources.panel_cabecera;
+            this.Panel_cabecera.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Panel_cabecera.Controls.Add(this.label1);
+            this.Panel_cabecera.Controls.Add(this.shapeContainer1);
+            this.Panel_cabecera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_cabecera.Location = new System.Drawing.Point(0, 0);
+            this.Panel_cabecera.Name = "Panel_cabecera";
+            this.Panel_cabecera.Size = new System.Drawing.Size(1122, 111);
+            this.Panel_cabecera.TabIndex = 48;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 27.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(285, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Crear nuevo reporte";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape7});
+            this.shapeContainer1.Size = new System.Drawing.Size(1122, 111);
+            this.shapeContainer1.TabIndex = 2;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape7
+            // 
+            this.rectangleShape7.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.rectangleShape7.Location = new System.Drawing.Point(1, 108);
+            this.rectangleShape7.Name = "rectangleShape7";
+            this.rectangleShape7.Size = new System.Drawing.Size(1121, 1);
+            // 
+            // Hacer_reportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Hacer_reportes";
+            this.ClientSize = new System.Drawing.Size(1122, 739);
+            this.Controls.Add(this.Panel_cabecera);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Hacer_reportes";
+            this.Panel_cabecera.ResumeLayout(false);
+            this.Panel_cabecera.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Panel_cabecera;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape7;
     }
 }
