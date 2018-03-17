249ba7a904c5adfe1e8cb3e89ec7c525b4a992dd
zoe1510 <patriciabermudez465@gmail.com>
2018-03-16 22:28:12 -0400

    agregar el form donde estará la lista de participantes

---
+++ UCS_NODO_FGC/Lista_participantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UCS_NODO_FGC
+{
+    public partial class Lista_participantes : Form
+    {
+        public Lista_participantes()
+        {
+            InitializeComponent();
+        }
+    }
+}
